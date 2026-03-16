1317687622f0eb28c54513bf00d15641e8bf7c49
Pedro Paulo Mardegam <pedro.paulo.mardegam@gmail.com>
2026-03-16 12:26:31 -0300

    Fix: título

--- Exercicio17.java
+++ Exercicio17.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Análise e Projeto Orienta a Objetos
+Professor: José Carlos Flores
+Turma: ESOFT3A
+Componentes:
+ 25169060-2 - Alex Rafael Ferreira
+ 26010487-2 - Anderson Kauã de Lima de Jesus Trindade
+ 25177941-2 - Juan Pyerre de Sousa Carvalho
+ 25181985-2 - Nathaly Yukie Otofuji Honda
+ 25181898-2 - Pedro Paulo Rodrigues Mardegam
+Data: 18 de Março de 2026
+Descritivo: Escreva um programa que calcule a soma de dois números
+*******************************************************************************/
+
 // Import que cria números aleatórios.
 import java.util.Random;
 import java.util.Scanner;
