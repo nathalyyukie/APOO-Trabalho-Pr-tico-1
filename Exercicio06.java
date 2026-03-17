d5aac5cf9e4a0a98a906d43aa0f2741f20888faf
this.juanpyerre <juan.pyerre777@gmail.com>
2026-03-17 16:55:35 -0300

    Update Exercicio06.java

--- Exercicio06.java
+++ Exercicio06.java
@@ -1,4 +1,4 @@
-/*
+/************************************************************************************************************
 Curso: Engenharia de Software
 Disciplina: Análise e Projeto Orienta a Objetos
 Professor: José Carlos Flores
@@ -15,29 +15,29 @@ Data: 18 de Março de 2026
 
 Descritivo: Escreva um programa que leia 5 números do usuário e os armazene em um array, depois exiba todos os
 números.
-*/
+**************************************************************************************************************/
 
-import java.util.Scanner;
+import java.util.Scanner;//importa a biblioteca do scanner
 
-public class Exercicio06 {
+public class Exercicio06 {//cria a class main/principal
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//cria o construtor
 
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);//cria o scanner
 
         // cria um array para guardar 5 números
         int[] numeros = new int[5];
 
         // lê os 5 números do usuário
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {//solicita 5 numero para o usuario usando um loop
             System.out.println("Digite um número:");
-            numeros[i] = entrada.nextInt();
+            numeros[i] = entrada.nextInt();//armazena os numeros dentro de cada casa do array
         }
 
         // mostra os números digitados
         System.out.println("Números digitados:");
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {//Criado um loop para printar os numeros do array.
             System.out.println(numeros[i]);
         }
 
